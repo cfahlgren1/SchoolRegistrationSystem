95e857c1ced62da221b5c1480902414421a2b36b
Caleb Fahlgren <calebfahlgren@protonmail.com>
2021-11-16 15:07:38 -0600

    add class grade and reporting capabilities

--- rsb/registrationsystem/models.py
+++ rsb/registrationsystem/models.py
@@ -1,34 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.deletion import DO_NOTHING
+from django.db.models.deletion import CASCADE, DO_NOTHING
 
-# Enrollment Summary
-class EnrollmentSummary(models.Model):
-    description = models.TextField()
-    date_created = models.DateTimeField(auto_now_add=True)
-    report = models.TextField() # list of student names in course
-    # have student name be added to enrollment when student adds
-
-    def __str__(self):
-        
-        return str(self.date)
-
-# Grade Report
-class GradeReport(models.Model):
-    description = models.TextField()
-    date = models.DateTimeField(auto_now_add=True)
-    report = models.TextField() # professors names per grade
-
-    def __str__(self):
-        return str(self.date)
-    
 # Professor Model
 class Professor(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    enrollmentSummaries = models.ManyToManyField(EnrollmentSummary, blank=True)
-    gradeReports = models.ManyToManyField(GradeReport, blank=True)
+    enrollmentSummaries = models.ManyToManyField('EnrollmentSummary', blank=True)
+    gradeReports = models.ManyToManyField('GradeReport', blank=True)
 
     def __str__(self):
         return self.name
@@ -37,11 +17,10 @@ class Professor(models.Model):
 class Course(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    professor = models.ForeignKey(Professor, on_delete=DO_NOTHING) #make this a one to many field
+    professor = models.ForeignKey(Professor, on_delete=DO_NOTHING)
     crn = models.IntegerField()
     description = models.TextField(blank=True)
-    enrollmentSummary = models.OneToOneField(EnrollmentSummary, on_delete=DO_NOTHING, blank=True, null=True)
-    gradeReport = models.OneToOneField(GradeReport, on_delete=DO_NOTHING, blank=True, null=True)
+    price = models.FloatField(default=0, blank=False)
 
     def __str__(self):
         return self.name
@@ -51,6 +30,76 @@ class Student(models.Model):
     name = models.CharField(max_length=100)
     user = models.OneToOneField(User, on_delete=models.CASCADE) # delete this student account, if user deletes account
     courses = models.ManyToManyField(Course, blank=True)
+    bill = models.FloatField(default=0)
+
+    # Calculate bill for student based on classes enrolled in
+    def calculate_bill(self):
+        amt = 0
+        for course in self.courses.all():
+            amt += course.price
+        self.bill = amt
+        self.save()
+
+    def __str__(self):
+        return self.name
+
+# Enrollment Summary
+class EnrollmentSummary(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.DO_NOTHING)
+    description = models.TextField(blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    report = models.TextField(blank=True) # list of student names in course
+
+    # Find students that are registered for course and build report
+    def generate_report(self):
+        course = models.ForeignKey(Course, on_delete=models.DO_NOTHING)
+        summary = ""
+        for student in Student.objects.all():
+            for course in student.courses.all():
+                if course == self.course:
+                    summary += student.name + "\n"
+                    break
+        self.report = summary
+        self.save()
+
+    def __str__(self):
+        return self.course.name
+
+# Student Class Model
+class StudentClass(models.Model):
+    name = models.CharField(max_length=100)
+    course = models.ForeignKey(Course, on_delete=DO_NOTHING)
+    student = models.ForeignKey(Student, on_delete=DO_NOTHING)
+    grade = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.student.name + " - " + self.course.name
+
+# Grade Report
+class GradeReport(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.DO_NOTHING)
+    description = models.TextField(blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    report = models.TextField(blank=True) # student grades
+    
+    def generate_report(self):
+        summary = ""
+        for student in Student.objects.all():
+            for course in student.courses.all():
+                if course == self.course:
+                    # student class
+                    summary += student.name + " - " + str(StudentClass.objects.get(student=student, course=self.course).grade) + "\n"
+                    break
+        self.report = summary
+        self.save()
+    
+    def __str__(self):
+        return self.course.name
+
+# Advisor
+class Advisor(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.OneToOneField(User, on_delete=models.CASCADE) # delete this student account, if user deletes account
 
     def __str__(self):
         return self.name
@@ -58,17 +107,23 @@ class Student(models.Model):
 # Drop Request
 class DropRequest(models.Model):
     course = models.OneToOneField(Course, on_delete=DO_NOTHING)
-    student = models.OneToOneField(Student, on_delete=DO_NOTHING)
+    student = models.ForeignKey(Student, on_delete=DO_NOTHING)
     
     # Drop student from course
-    def acceptRequest(self):
+    def accept_request(self):
         # remove student from course
         self.student.courses.remove(self.course)
+        self.student.calculate_bill()
+
+        # delete student class
+        student_class = StudentClass.objects.get_or_create(name=self.course.name,course=self.course, student=self.student)[0]
+        student_class.delete()
+
         #remove request when done
         super().delete()
     
     # Deny Request
-    def denyRequest(self):
+    def deny_request(self):
         super().delete()
     
     def __str__(self):
@@ -77,17 +132,23 @@ class DropRequest(models.Model):
 # Add Request
 class AddRequest(models.Model):
     course = models.OneToOneField(Course, on_delete=DO_NOTHING)
-    student = models.OneToOneField(Student, on_delete=DO_NOTHING)
+    student = models.ForeignKey(Student, on_delete=DO_NOTHING)
 
     # Add student to course
-    def acceptRequest(self):
+    def accept_request(self):
         # add student to course
         self.student.courses.add(self.course)
+        self.student.calculate_bill()
+
+        # create student class
+        student_class = StudentClass.objects.get_or_create(name=self.course.name,course=self.course, student=self.student)[0]
+        student_class.save()
+
         # delete request when done
         super().delete()
 
     # Delete Request 
-    def denyRequest(self):
+    def deny_request(self):
         super().delete()
 
     def __str__(self):
